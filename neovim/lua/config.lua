6ac6d6765ac17538a89a69e9b63d857f4f35cbcf
Emil Ohlsson <kotte@kottland.net>
2021-08-20 20:42:13 +0200

    fix thingy

--- neovim/lua/config.lua
+++ neovim/lua/config.lua
@@ -6,7 +6,7 @@ local on_attach = function(client, bufnr)
 		vim.api.nvim_buf_set_keymap(bufnr, ...)
 	end
 	local opts = { noremap = true, silent = true }
-	buf_set_keymap('n', '<leader>rn', '<cmd> lua vim.lsp.buf.rename()<CR>', opts)
+	buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 	buf_set_keymap('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 	buf_set_keymap('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 	buf_set_keymap('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
